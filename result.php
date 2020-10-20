2a42a383f0866d8cd7389d423f9d77deae673023
yasu-pro <cb1-y.s@docomo.ne.jp>
2020-10-21 05:58:11 +0900

    add img,result.php,SQLコマンド,html

---
+++ result.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>検索結果 ページ</title>
+</head>
+<body>
+    <?php
+        
+    ?>
+</body>
+</html>
