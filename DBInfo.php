23188adda22176e3cea7589b19a7e31b06758958
yasu-pro <cb1-y.s@docomo.ne.jp>
2020-10-22 09:47:07 +0900

    DBInfo.php 追加,result.php 編集

---
+++ DBInfo.php
@@ -0,0 +1,6 @@
+<?php
+class DBInfo {
+    const DSN='mysql:dataname=usedcar;host=localhost;';
+    const USER='root';
+    const PASSWORD='root';
+}
--- result.php
+++ result.php
@@ -7,7 +7,72 @@
 </head>
 <body>
     <?php
-        
+        try {
+            //DB接続情報
+            require_once('./DBInfo.php');
+            //PDO::constructでRDBMSに接続
+            $pdo = new PDO(DBInfo::DSN,DBInfo::USER,DBInfo::PASSWORD);
+            echo('接続しました<br/>');
+        } catch (PDOException $e) {
+            $code = $e->getcode();
+            $message = $e->getMessage();
+            echo("{$code}/{$message}<br/>");
+        }//nullを代入して切断する
+        $pdo = null;
+        echo("切断しました<br/>");
+
+        if(isset($_GET['id'])==true){
+            $id = $_GET['id'];
+        }
+        else{
+            echo('読み込めませんでした');
+        }
+
+        switch ($id) {
+            case "all":
+                echo('goodmorning');
+                break;
+
+                case "benz":
+                echo('goodmorning');
+                break;
+
+                case "bmw":
+                echo('goodmorning');
+                break;
+
+                case "vw":
+                echo('goodmorning');
+                break;
+
+                case "audi":
+                echo('goodmorning');
+                break;
+
+                case "mini":
+                echo('goodmorning');
+                break;
+
+                case "porsche":
+                echo('goodmorning');
+                break;
+
+                case "volvo":
+                echo('goodmorning');
+                break;
+                
+                case "peugeot":
+                echo('goodmorning');
+                break;
+    
+                case "lr":
+                echo('goodmorning');
+                break;
+                                     
+            default:
+                ehcho('眠い');
+                break;
+        }
     ?>
 </body>
 </html>
