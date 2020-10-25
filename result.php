c646b3c1d1e605154ce1d6823c38f44ef3651954
yasu-pro <cb1-y.s@docomo.ne.jp>
2020-10-25 16:33:54 +0900

    fixed result.php

--- result.php
+++ result.php
@@ -1,25 +1,26 @@
 <?php
-    if(isset($_GET['id'])==true){
+    // if(isset($_GET['id'])==true){
+    //     $id = $_GET['id'];
+    // }
+
+    // if (isset($_GET['low_price'])==true) {
+    //     $low_price = $_GET['low_price'];
+    // }
+
+    // if (isset($_GET['upper_price'])==true) {
+    //     $upper_price = $_GET['upper_price'];
+    // }
+    // else{
+    //     echo('読み込めませんでした');
+    // }
+
+    if(isset($_GET['id'])==true ||
+        isset($_GET['low_price'])==true ||
+         isset($_GET['upper_price'])==true){
         $id = $_GET['id'];
-    }
-    else{
-        echo('読み込めませんでした');
-    }
-
-    if (isset($_GET['low_price'])==true) {
         $low_price = $_GET['low_price'];
-    }
-    else{
-        echo('読み込めませんでした');
-    }
-
-    if (isset($_GET['upper_price'])==true) {
         $upper_price = $_GET['upper_price'];
     }
-    else{
-        echo('読み込めませんでした');
-    }
-
 
     try {
         //DB接続情報
@@ -192,13 +193,56 @@
                 break;
                                     
                 default:
-                ehcho('データがありません');
+                echo('データがありません');
                 break;
-            }
+        }
 
         //価格から探す
-        
+        if ($low_price < $upper_price) {
+            $sql = "SELECT * FROM usedcarInfo WHERE price BETWEEN $low_price AND $upper_price ORDER BY price ASC";
+            //参照系SQLを発行
+            $statement = $pdo -> query($sql);
+            //データの取得
+            while ($row = $statement -> fetch()) {
+                echo($row[0]);
+                echo($row[1]);
+                echo($row[2]);
+                echo("/{$row[3]}/");
+                echo($row[4]);
+                echo("{$row[5]}<br/>");
+            }
+            
+        }elseif ($low_price == $upper_price) {
+            $sql = "SELECT * FROM usedcarInfo WHERE price = $low_price ORDER BY price ASC";
+            //参照系SQLを発行
+            $statement = $pdo -> query($sql);
+            //データの取得
+            while ($row = $statement -> fetch()) {
+                echo($row[0]);
+                echo($row[1]);
+                echo($row[2]);
+                echo("/{$row[3]}/");
+                echo($row[4]);
+                echo("{$row[5]}<br/>");
+            }
+        }
+        // elseif($upper_price == "infinite") {
+        //     $sql = "SELECT * FROM usedcarInfo WHERE price >= $upper_price";
+        //     //参照系SQLを発行
+        //     $statement = $pdo -> query($sql);
+        //     //データの取得
+        //     while ($row = $statement -> fetch()) {
+        //         echo($row[0]);
+        //         echo($row[1]);
+        //         echo($row[2]);
+        //         echo("/{$row[3]}/");
+        //         echo($row[4]);
+        //         echo("{$row[5]}<br/>");
+        //     }
+        // }
 
+        //走行距離から探す
+        
         $pdo = null;
         echo("切断しました<br/>");
 
