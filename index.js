4b2a71391fa9b99862b9fb436930b020909b2262
yasu-pro <cb1-y.s@docomo.ne.jp>
2020-10-25 18:03:41 +0900

    Add click event to index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 "use strict";
+
+const mileage__radios = document.querySelectorAll("div.mileage__radio > input");
+
+for (let i = 1; i <= mileage__radios.length; i++) {
+  mileage__radios[i - 1].addEventListener("click", (event) => {
+    const mileage__radio__Id = document.getElementById("mileage_" + i);
+    submit();
+  });
+}
+
+function submit() {
+  document.mileage.action = "./result.php";
+  document.mileage.method = "get";
+  document.mileage.submit();
+}
