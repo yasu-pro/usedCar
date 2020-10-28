fd406ca63c43e6825619d092b896db24f6b8bbc4
yasu-pro <cb1-y.s@docomo.ne.jp>
2020-10-28 20:53:35 +0900

    Display image from DB in index.php

--- result.php
+++ result.php
@@ -20,366 +20,115 @@
         echo('接続しました<br/>');
 
         //メーカーから探す
-        switch ($id) {
-            case "all":
+        if(isset($id)==true){
+            if ($id == "all") {
                 //参照系SQL
                 $sql = "SELECT * FROM usedcarInfo";        
                 //参照系SQLを発行
                 $statement = $pdo -> query($sql);
-                //データの取得
-                while ($row = $statement -> fetch()) {
-                    echo($row[0]);
-                    echo($row[1]);
-                    echo($row[2]);
-                    echo($row[3]);
-                    echo($row[4]);
-                    echo("{$row[5]}<br/>");
-                }
-                break;
-    
-                case "benz":
+            }
+            elseif($id != "all") {
                 //参照系SQL
-                $sql = "SELECT * FROM usedcarInfo WHERE maker = 'benz'";        
-                //参照系SQLを発行
-                $statement = $pdo -> query($sql);
-                //データの取得
-                while ($row = $statement -> fetch()) {
-                    echo($row[0]);
-                    echo($row[1]);
-                    echo($row[2]);
-                    echo($row[3]);
-                    echo($row[4]);
-                    echo("{$row[5]}<br/>");
-                }
-                break;
-    
-                case "bmw":
-                //参照系SQL
-                $sql = "SELECT * FROM usedcarInfo WHERE maker = 'bmw'";        
-                //参照系SQLを発行
-                $statement = $pdo -> query($sql);
-                //データの取得
-                while ($row = $statement -> fetch()) {
-                    echo($row[0]);
-                    echo($row[1]);
-                    echo($row[2]);
-                    echo($row[3]);
-                    echo($row[4]);
-                    echo("{$row[5]}<br/>");
-                }
-                break;
-    
-                case "vw":
-                //参照系SQL
-                $sql = "SELECT * FROM usedcarInfo WHERE maker = 'vw'";        
-                //参照系SQLを発行
-                $statement = $pdo -> query($sql);
-                //データの取得
-                while ($row = $statement -> fetch()) {
-                    echo($row[0]);
-                    echo($row[1]);
-                    echo($row[2]);
-                    echo($row[3]);
-                    echo($row[4]);
-                    echo("{$row[5]}<br/>");
-                }
-                break;
-    
-                case "audi":
-                //参照系SQL
-                $sql = "SELECT * FROM usedcarInfo WHERE maker = 'audi'";        
-                //参照系SQLを発行
-                $statement = $pdo -> query($sql);
-                //データの取得
-                while ($row = $statement -> fetch()) {
-                    echo($row[0]);
-                    echo($row[1]);
-                    echo($row[2]);
-                    echo($row[3]);
-                    echo($row[4]);
-                    echo("{$row[5]}<br/>");
-                }
-                break;
-    
-                case "mini":
-                //参照系SQL
-                $sql = "SELECT * FROM usedcarInfo WHERE maker = 'mini'";        
-                //参照系SQLを発行
-                $statement = $pdo -> query($sql);
-                //データの取得
-                while ($row = $statement -> fetch()) {
-                    echo($row[0]);
-                    echo($row[1]);
-                    echo($row[2]);
-                    echo($row[3]);
-                    echo($row[4]);
-                    echo("{$row[5]}<br/>");
-                }
-                break;
-    
-                case "porsche":
-                //参照系SQL
-                $sql = "SELECT * FROM usedcarInfo WHERE maker = 'porsche'";        
-                //参照系SQLを発行
-                $statement = $pdo -> query($sql);
-                //データの取得
-                while ($row = $statement -> fetch()) {
-                    echo($row[0]);
-                    echo($row[1]);
-                    echo($row[2]);
-                    echo($row[3]);
-                    echo($row[4]);
-                    echo("{$row[5]}<br/>");
-                }
-                break;
-    
-                case "volvo":
-                //参照系SQL
-                $sql = "SELECT * FROM usedcarInfo WHERE maker = 'volvo'";        
-                //参照系SQLを発行
-                $statement = $pdo -> query($sql);
-                //データの取得
-                while ($row = $statement -> fetch()) {
-                    echo($row[0]);
-                    echo($row[1]);
-                    echo($row[2]);
-                    echo($row[3]);
-                    echo($row[4]);
-                    echo("{$row[5]}<br/>");
-                }
-                break;
-                
-                case "peugeot":
-                //参照系SQL
-                $sql = "SELECT * FROM usedcarInfo WHERE maker = 'peugeot'";        
-                //参照系SQLを発行
-                $statement = $pdo -> query($sql);
-                //データの取得
-                while ($row = $statement -> fetch()) {
-                    echo($row[0]);
-                    echo($row[1]);
-                    echo($row[2]);
-                    echo($row[3]);
-                    echo($row[4]);
-                    echo("{$row[5]}<br/>");
-                }
-                break;
-    
-                case "lr":
-                //参照系SQL
-                $sql = "SELECT * FROM usedcarInfo WHERE maker = 'lr'";        
-                //参照系SQLを発行
-                $statement = $pdo -> query($sql);
-                //データの取得
-                while ($row = $statement -> fetch()) {
-                    echo($row[0]);
-                    echo($row[1]);
-                    echo($row[2]);
-                    echo($row[3]);
-                    echo($row[4]);
-                    echo("{$row[5]}<br/>");
-                }
-                break;
-                                    
-                default:
-                echo('データがありません');
-                break;
+                $sql = "SELECT * FROM usedcarInfo WHERE maker = ?";    
+                //参照系SQLを発行    
+                $statement = $pdo ->prepare($sql);
+                $statement -> bindValue(1,$id);
+                $statement -> execute();
+            }else {
+                echo('車がありません');
+            }
         }
 
         //価格から探す
-        if ($low_price < $upper_price) {
-            $sql = "SELECT * FROM usedcarInfo WHERE price BETWEEN '" . $low_price . "' AND '" . $upper_price . "' ORDER BY price";
-            //参照系SQLを発行
-            $statement = $pdo -> query($sql);
-            //データの取得
-            while ($row = $statement -> fetch()) {
-                echo($row[0]);
-                echo($row[1]);
-                echo($row[2]);
-                echo("/{$row[3]}/");
-                echo($row[4]);
-                echo("{$row[5]}<br/>");
-            }
-            
-        }elseif ($low_price == $upper_price) {
-            $sql = "SELECT * FROM usedcarInfo WHERE price = '" . $low_price . "' ORDER BY price";
-            //参照系SQLを発行
-            $statement = $pdo -> query($sql);
-            //データの取得
-            while ($row = $statement -> fetch()) {
-                echo($row[0]);
-                echo($row[1]);
-                echo($row[2]);
-                echo("/{$row[3]}/");
-                echo($row[4]);
-                echo("{$row[5]}<br/>");
+        if(isset($low_price)==true && isset($upper_price)==true){
+            if ($low_price < $upper_price || $low_price == $upper_price) {
+                $sql = "SELECT * FROM usedcarInfo WHERE price BETWEEN ? AND ? ORDER BY price";
+                //参照系SQLを発行
+                $statement = $pdo -> prepare($sql);
+                $statement -> bindValue(1,$low_price);
+                $statement -> bindValue(2,$upper_price); 
+                $statement -> execute();
+            }else{
+                echo("車がありません");
             }
         }
-
         //走行距離から探す
-        if ($mile == "1") {
-            $sql = "SELECT * FROM usedcarInfo WHERE mileage <= 1 ORDER BY mileage ASC";
-            //参照系SQLを発行
-            $statement = $pdo -> query($sql);
-            //データの取得
-            while ($row = $statement -> fetch()) {
-                echo($row[0]);
-                echo($row[1]);
-                echo($row[2]);
-                echo("/{$row[3]}/");
-                echo($row[4]);
-                echo("{$row[5]}<br/>");
+        if (isset($mile) == true) {
+            if($mile =="1"){
+                $sql = "SELECT * FROM usedcarInfo WHERE mileage <= 1 ORDER BY mileage ASC";
+                //参照系SQLを発行
+                $statement = $pdo -> query($sql);
+            }elseif ($mile =="15") {
+                $sql = "SELECT * FROM usedcarInfo WHERE mileage >= 15 ORDER BY mileage ASC";
+                //参照系SQLを発行
+                $statement = $pdo -> query($sql);
             }
-
-        }elseif($mile == "1~3") {
-            $sql = "SELECT * FROM usedcarInfo WHERE mileage >= 1 AND mileage <=3 ORDER BY mileage ASC";
-            //参照系SQLを発行
-            $statement = $pdo -> query($sql);
-            //データの取得
-            while ($row = $statement -> fetch()) {
-                echo($row[0]);
-                echo($row[1]);
-                echo($row[2]);
-                echo("/{$row[3]}/");
-                echo($row[4]);
-                echo("{$row[5]}<br/>");
-            }
-        }elseif($mile == "3~5") {
-            $sql = "SELECT * FROM usedcarInfo WHERE mileage >= 3 AND mileage <=5 ORDER BY mileage ASC";
-            //参照系SQLを発行
-            $statement = $pdo -> query($sql);
-            //データの取得
-            while ($row = $statement -> fetch()) {
-                echo($row[0]);
-                echo($row[1]);
-                echo($row[2]);
-                echo("/{$row[3]}/");
-                echo($row[4]);
-                echo("{$row[5]}<br/>");
-            }
-        }elseif($mile == "5~10") {
-            $sql = "SELECT * FROM usedcarInfo WHERE mileage >= 5 AND mileage <=10 ORDER BY mileage ASC";
-            //参照系SQLを発行
-            $statement = $pdo -> query($sql);
-            //データの取得
-            while ($row = $statement -> fetch()) {
-                echo($row[0]);
-                echo($row[1]);
-                echo($row[2]);
-                echo("/{$row[3]}/");
-                echo($row[4]);
-                echo("{$row[5]}<br/>");
-            }
-        }elseif($mile == "10~15") {
-            $sql = "SELECT * FROM usedcarInfo WHERE mileage >= 10 AND mileage <=15 ORDER BY mileage ASC";
-            //参照系SQLを発行
-            $statement = $pdo -> query($sql);
-            //データの取得
-            while ($row = $statement -> fetch()) {
-                echo($row[0]);
-                echo($row[1]);
-                echo($row[2]);
-                echo("/{$row[3]}/");
-                echo($row[4]);
-                echo("{$row[5]}<br/>");
-            }
-        }elseif($mile == "15") {
-            $sql = "SELECT * FROM usedcarInfo WHERE mileage >= 15 ORDER BY mileage ASC";
-
-            //参照系SQLを発行
-            $statement = $pdo -> query($sql);
-            //データの取得
-            while ($row = $statement -> fetch()) {
-                echo($row[0]);
-                echo($row[1]);
-                echo($row[2]);
-                echo("/{$row[3]}/");
-                echo($row[4]);
-                echo("{$row[5]}<br/>");
+            else{
+                $sql = "SELECT * FROM usedcarInfo WHERE mileage >= ? AND mileage <= ? ORDER BY mileage ASC";
+                $statement = $pdo -> prepare($sql);
+           
+                switch ($mile) {
+                    case "1~3":
+                        $statement -> bindValue(1,1);
+                        $statement -> bindValue(2,3);
+                    break;
+                    case "3~5":
+                        $statement -> bindValue(1,3);
+                        $statement -> bindValue(2,5);
+                    break;
+                    case "5~10":
+                        $statement -> bindValue(1,5);
+                        $statement -> bindValue(2,10);
+                    break;
+                    case "10~15":
+                        $statement -> bindValue(1,10);
+                        $statement -> bindValue(2,15);
+                    break;
+                    default:
+                        echo("車がありません");
+                    break;
+                }
+                $statement -> execute();
             }
         }
             //経過年数から探す
-        if ($year == "1") {
-            $sql = "SELECT * FROM usedcarInfo WHERE year <= 1 ORDER BY year ASC";
-            //参照系SQLを発行
-            $statement = $pdo -> query($sql);
-            //データの取得
-            while ($row = $statement -> fetch()) {
-                echo($row[0]);
-                echo($row[1]);
-                echo($row[2]);
-                echo("/{$row[3]}/");
-                echo($row[4]);
-                echo("{$row[5]}<br/>");
+        if (isset($year) == true) {
+            if($year =="1"){
+                $sql = "SELECT * FROM usedcarInfo WHERE year <= 1 ORDER BY year ASC";
+                //参照系SQLを発行
+                $statement = $pdo -> query($sql);
+            }elseif ($year =="13") {
+                $sql = "SELECT * FROM usedcarInfo WHERE year >= 13 ORDER BY year ASC";
+                //参照系SQLを発行
+                $statement = $pdo -> query($sql);
             }
-
-        }elseif($year == "1~3") {
-            $sql = "SELECT * FROM usedcarInfo WHERE year >= 1 AND year <= 3 ORDER BY year ASC";
-            //参照系SQLを発行
-            $statement = $pdo -> query($sql);
-            //データの取得
-            while ($row = $statement -> fetch()) {
-                echo($row[0]);
-                echo($row[1]);
-                echo($row[2]);
-                echo("/{$row[3]}/");
-                echo($row[4]);
-                echo("{$row[5]}<br/>");
+            else{
+                $sql = "SELECT * FROM usedcarInfo WHERE year >= ? AND year <= ? ORDER BY year ASC";
+                $statement = $pdo -> prepare($sql);
+            
+                switch ($year) {
+                    case "1~3":
+                        $statement -> bindValue(1,1);
+                        $statement -> bindValue(2,3);
+                    break;
+                    case "3~5":
+                        $statement -> bindValue(1,3);
+                        $statement -> bindValue(2,5);
+                    break;
+                    case "5~10":
+                        $statement -> bindValue(1,5);
+                        $statement -> bindValue(2,10);
+                    break;
+                    case "10~15":
+                        $statement -> bindValue(1,10);
+                        $statement -> bindValue(2,15);
+                    break;
+                    default:
+                        echo("車がありません");
+                    break;
+                }
+                $statement -> execute();
             }
-        }elseif($year == "4~5") {
-            $sql = "SELECT * FROM usedcarInfo WHERE year >= 4 AND year <= 5 ORDER BY year ASC";
-            //参照系SQLを発行
-            $statement = $pdo -> query($sql);
-            //データの取得
-            while ($row = $statement -> fetch()) {
-                echo($row[0]);
-                echo($row[1]);
-                echo($row[2]);
-                echo("/{$row[3]}/");
-                echo($row[4]);
-                echo("{$row[5]}<br/>");
-            }
-        }elseif($year == "6~8") {
-            $sql = "SELECT * FROM usedcarInfo WHERE year >= 6 AND year <= 8 ORDER BY year ASC";
-            //参照系SQLを発行
-            $statement = $pdo -> query($sql);
-            //データの取得
-            while ($row = $statement -> fetch()) {
-                echo($row[0]);
-                echo($row[1]);
-                echo($row[2]);
-                echo("/{$row[3]}/");
-                echo($row[4]);
-                echo("{$row[5]}<br/>");
-            }
-        }elseif($year == "9~12") {
-            $sql = "SELECT * FROM usedcarInfo WHERE year >= 9 AND year <= 12 ORDER BY year ASC";
-            //参照系SQLを発行
-            $statement = $pdo -> query($sql);
-            //データの取得
-            while ($row = $statement -> fetch()) {
-                echo($row[0]);
-                echo($row[1]);
-                echo($row[2]);
-                echo("/{$row[3]}/");
-                echo($row[4]);
-                echo("{$row[5]}<br/>");
-            }
-        }elseif($year == "13") {
-            $sql = "SELECT * FROM usedcarInfo WHERE year >= 13 ORDER BY year ASC";
-
-            //参照系SQLを発行
-            $statement = $pdo -> query($sql);
-            //データの取得
-            while ($row = $statement -> fetch()) {
-                echo($row[0]);
-                echo($row[1]);
-                echo($row[2]);
-                echo("/{$row[3]}/");
-                echo($row[4]);
-                echo("{$row[5]}<br/>");
-            }
-
         }
         $pdo = null;
         echo("切断しました<br/>");
@@ -392,7 +141,7 @@
         echo("{$code}/{$message}<br/>");
         //nullを代入して切断する
         $pdo = null;
-        echo("切断しました<br/>");
+        echo("切断しました(error)<br/>");
 
     }
 ?>
@@ -406,8 +155,20 @@
 </head>
 <!-- 0:pass 1:number 2:maker 3:price 4:mileage 5:year -->
 <body>
+    <div class="result">
 <?php
-
-    ?>
+    echo("<div>");
+            //データの取得
+            while ($row = $statement -> fetch()) {
+                echo("<img src=./{$row[0]} alt={$row[2]}>");
+                echo("{$row[1]}/");
+                echo("{$row[2]}/");
+                echo("{$row[3]}/");
+                echo("{$row[4]}/");
+                echo("{$row[5]}<br/>");
+            }
+    echo("</div>");
+?>
+    </div>
 </body>
 </html>
