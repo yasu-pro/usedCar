a3048f9759bc15c99689ef745ecad9c751b767e9
yasu-pro <cb1-y.s@docomo.ne.jp>
2020-11-10 17:28:48 +0900

    Added rental server information to DBInfo and code to index.html to prevent crawlers from patrol

--- DBInfo.php
+++ DBInfo.php
@@ -1,6 +1,6 @@
 <?php
 class DBInfo {
-    const DSN='mysql:dbname=usedcar;host=localhost;';
-    const USER='root';
-    const PASSWORD='root';
+    const DSN='mysql:dbname=yasutmy_usedcar;host=mysql8055.xserver.jp;';
+    const USER='yasutmy_usedcar';
+    const PASSWORD='03o8y.fk.e';
 }
--- result.php
+++ result.php
@@ -151,6 +151,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name='robots' content='noindex,nofollow' />
     <link rel="stylesheet" href="./css/result.css">
     <title>検索結果 ページ</title>
 </head>
